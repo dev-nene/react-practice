e7c8a2c650eefa7ef87a42cffca7f9a7c5dd057d
Nemanja Pavlovic <268587348+dev-nene@users.noreply.github.com>
2026-05-15 08:16:01 +0200

    Add Button component with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import Button from "./components/Button";
+import Greeting from "./components/Greeting";
 
 function App() {
+  const handleButtonClick = (url) => {
+    window.location.href = url;
+  };
+
   return (
-    <h1>Hello World!</h1>
-  )
+    <>
+      <h1>Hello World!</h1>
+      <Greeting />
+      <Button />
+      <Button
+        text="Click me twice"
+        color="red"
+        fontSize="24"
+        handleClick={() => handleButtonClick("https://www.google.com")}
+      />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+export default function Button({
+  text = "Click Me!",
+  color = "blue",
+  fontSize = "12",
+  handleClick,
+}) {
+  const buttonStyle = {
+    color: color,
+    fontSize: fontSize + "px",
+  };
+  return (
+    <button onClick={handleClick} style={buttonStyle}>
+      {text}
+    </button>
+  );
+}
