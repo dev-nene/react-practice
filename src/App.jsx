5eb416173f07dff376e8454d14f1edfd4b713af7
Nemanja Pavlovic <268587348+dev-nene@users.noreply.github.com>
2026-05-30 18:39:45 +0200

    Practice React state

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
+import { useState } from "react";
 import Button from "./components/Button";
 import Greeting from "./components/Greeting";
 
+const COLORS = ["pink", "green", "blue", "yellow", "purple"];
+
 function App() {
-  const handleButtonClick = (url) => {
-    window.location.href = url;
+  const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [counter, setCounter] = useState(0);
+
+  const onButtonClick = (color) => () => {
+    setBackgroundColor(color);
+    setCounter((prev) => prev + 1);
   };
 
   return (
-    <>
-      <h1>Hello World!</h1>
-      <Greeting />
-      <Button />
-      <Button
-        text="Click me twice"
-        color="red"
-        fontSize="24"
-        handleClick={() => handleButtonClick("https://www.google.com")}
-      />
-    </>
+    <div
+      className="App"
+      style={{
+        backgroundColor,
+      }}
+    >
+      <h2>{counter}</h2>
+      {COLORS.map((color) => (
+        <button
+          type="button"
+          key={color}
+          onClick={onButtonClick(color)}
+          className={backgroundColor === color ? "selected" : ""}
+        >
+          {color}
+        </button>
+      ))}
+    </div>
   );
 }
-
 export default App;
